aca2f3b1a1e2acab6f01bfa4c2a42df533605ebe
Emmanuel <emmanuelm41@gmail.com>
2021-06-25 12:57:16 -0300

    remove useless cmd on entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,15 +1,5 @@
 #!/bin/bash
 
-echo "Setting ownership/permissions on ${BARMAN_DATA_DIR} and ${BARMAN_LOG_DIR}"
-
-install -d -m 0700 -o barman -g barman ${BARMAN_DATA_DIR}
-install -d -m 0755 -o barman -g barman ${BARMAN_LOG_DIR}
-
-# Examples
-# echo "Checking/Creating replication slot"
-# barman replication-status ${DB_HOST} --minimal --target=wal-streamer | grep barman || barman receive-wal --create-slot ${DB_HOST}
-# barman replication-status ${DB_HOST} --minimal --target=wal-streamer | grep barman || barman receive-wal --reset ${DB_HOST}
-
 if [[ -f /home/barman/.ssh/id_rsa ]]; then
     echo "Setting up Barman private key"
     chmod 700 ~barman/.ssh
